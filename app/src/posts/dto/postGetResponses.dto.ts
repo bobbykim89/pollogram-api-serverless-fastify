6d6223b59782cbf8ae9766344598c61d88cce630
Sihun Kim <skim585@asu.edu>
2025-01-11 22:40:31 -0700

    feat(app/): establish dto
    establish dto's for posts: postResponse, postListResponse, postLikeObject, postDetailResponse.

--- app/src/posts/dto/postGetResponses.dto.ts
+++ app/src/posts/dto/postGetResponses.dto.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod'
+import { profileResponseSchema } from '../../profile/dto'
 
 export const postResponseSchema = z.object({
   id: z.number(),
@@ -9,7 +10,24 @@ export const postResponseSchema = z.object({
   updated_at: z.date(),
 })
 
+export const postListResponseSchema = z.array(postResponseSchema)
+
+export const postLikeObjectSchema = z.object({
+  profile_id: z.number(),
+  post_id: z.number(),
+})
+
+export const postDetailResponseSchema = postResponseSchema.extend({
+  userProfile: profileResponseSchema,
+  // TODO: add proper typedef for comments comments dto's are established.
+  comments: z.array(z.string()),
+  liked_by: z.array(postLikeObjectSchema),
+})
+
 export type PostResponse = z.infer<typeof postResponseSchema>
+export type PostListResponse = z.infer<typeof postListResponseSchema>
+export type PostLikeObject = z.infer<typeof postLikeObjectSchema>
+export type PostDetailResponse = z.infer<typeof postDetailResponseSchema>
 
 // model Post {
 //   id Int @id @default(autoincrement())
@@ -23,4 +41,13 @@ export type PostResponse = z.infer<typeof postResponseSchema>
 //   updated_at DateTime @updatedAt
 
 //   @@map("posts")
+// }
+
+// model PostLike {
+//   profile_id Int
+//   post_id Int
+//   profile Profile @relation(fields: [profile_id], references: [id], onDelete: Cascade)
+//   liked_post Post @relation(fields: [post_id], references: [id], onDelete: Cascade)
+
+//   @@id([profile_id, post_id])
 // }
