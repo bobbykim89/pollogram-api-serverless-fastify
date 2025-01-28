fb29a4cf9c0e48d2cd49d281266029926ef7d089
Sihun Kim <skim585@asu.edu>
2025-01-28 10:09:00 -0700

    feat(comments/,profile/): code cleanup
    cleanup the lines that are no longer needed (from dto files).
    
    develop

--- src/comments/dto/commentGetResponse.dto.ts
+++ src/comments/dto/commentGetResponse.dto.ts
@@ -1,17 +1,5 @@
 import { z } from 'zod'
-import {
-  profileResponseSchema,
-  postResponseSchema,
-  commentResponseSchema,
-} from '../../common/dto'
-
-// export const commentResponseSchema = z.object({
-//   id: z.number(),
-//   text: z.string(),
-//   profile_id: z.number(),
-//   post_id: z.number(),
-//   created_at: z.date(),
-// })
+import { profileResponseSchema, commentResponseSchema } from '../../common/dto'
 
 export const commentLikeObjectSchema = z.object({
   profile_id: z.number(),
@@ -26,24 +14,3 @@ export const commentDetailResponseSchema = commentResponseSchema.extend({
 export type CommentResponse = z.infer<typeof commentResponseSchema>
 export type CommentLikeObject = z.infer<typeof commentLikeObjectSchema>
 export type CommentDetailResponse = z.infer<typeof commentDetailResponseSchema>
-
-// model Comment {
-//   id Int @id @default(autoincrement())
-//   text String
-//   profile_id Int
-//   post_id Int
-//   user_profile Profile @relation(fields: [profile_id], references: [id], onDelete: Cascade)
-//   post Post @relation(fields: [post_id], references: [id], onDelete: Cascade)
-//   liked_by CommentLike[]
-//   created_at DateTime @default(now())
-
-//   @@map("comments")
-// }
-// model CommentLike {
-//   profile_id Int
-//   comment_id Int
-//   profile Profile @relation(fields: [profile_id], references: [id], onDelete: Cascade)
-//   liked_comment Comment @relation(fields: [comment_id], references: [id], onDelete: Cascade)
-
-//   @@id([profile_id, comment_id])
-// }
--- src/profile/dto/profileGetResponses.dto.ts
+++ src/profile/dto/profileGetResponses.dto.ts
@@ -22,30 +22,3 @@ export type ProfileResponse = z.infer<typeof profileResponseSchema>
 export type ProfileListResponse = z.infer<typeof profileListResponseSchema>
 export type FollowObject = z.infer<typeof followObjectSchema>
 export type ProfileDetailResponse = z.infer<typeof profileDetailResponseSchema>
-
-// model Follow {
-//   followed_by_id Int
-//   following_id Int
-//   followed_by Profile @relation("followed_by", fields: [followed_by_id], references: [id], onDelete: Cascade)
-//   following Profile @relation("following", fields: [following_id], references: [id], onDelete: Cascade)
-
-//   @@id([following_id, followed_by_id])
-// }
-
-// model Profile {
-//   id Int @id @default(autoincrement())
-//   username String @unique
-//   image_id String?
-//   profile_description String?
-//   user User @relation(fields: [user_id], references: [id])
-//   user_id Int @unique
-//   posts Post[]
-//   comments Comment[]
-//   following Follow[] @relation("following")
-//   followed_by Follow[] @relation("followed_by")
-//   liked_posts PostLike[]
-//   liked_comments CommentLike[]
-//   created_at DateTime @default(now())
-//   updated_at DateTime @updatedAt
-//   @@map("profiles")
-// }
